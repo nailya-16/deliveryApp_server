7a7b2aadae65bcc35a2ae1017b7f2421f489b6cf
Nelya <nelya0816@gmail.com>
2025-06-29 03:20:11 +0300

    Added modules, services and controllers users and auth. Updated AppModule to connect UsersModule and AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PizzaModule } from './pizza/pizza.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OrdersModule } from './orders/orders.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PizzaModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/pizza'),
+    OrdersModule,
+    AuthModule,
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
