8b95eaa1dd31923e33fc2a9da597cf30298a03fd
Nelya <nelya0816@gmail.com>
2025-06-29 03:25:46 +0300

    Updated ESLint configuration: fixed project and tsconfigRootDir in parserOptions, added rule 'prettier/prettier': 'off'.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,8 +19,8 @@ export default tseslint.config(
       },
       sourceType: 'commonjs',
       parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
       },
     },
   },
@@ -28,7 +28,8 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': 'off'
     },
   },
 );
