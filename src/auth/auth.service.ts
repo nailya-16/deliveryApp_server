55e6af6985ddb2f6c25aa8bde5ad7ba6cc41f101
Nelya <nelya0816@gmail.com>
2025-07-01 01:19:06 +0300

    Added methods for user registration and login, hashing passwords with bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async register(name: string, email: string, password: string) {
+    const exists = await this.usersService.getUserByEmail(email);
+    if (exists) throw new ConflictException('Email уже зарегистрирован');
+    const hash = await bcrypt.hash(password, 10);
+    const user = await this.usersService.sendUser({ name, email, password: hash });
+    // Удаляем пароль из ответа
+    const { password: _, ...safeUser } = user.toObject();
+    return safeUser;
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.usersService.getUserByEmail(email);
+    if (!user) throw new UnauthorizedException('Неверный email или пароль');
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) throw new UnauthorizedException('Неверный email или пароль');
+
+
+    const { password: _, _id, ...rest } = user.toObject();
+    return { id: _id, ...rest };
+  }
+}
