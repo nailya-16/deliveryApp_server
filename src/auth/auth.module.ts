f89d41184b91c6f016a511175afba8c730b1146b
Nelya <nelya0816@gmail.com>
2025-07-19 11:58:07 +0300

    Added JwtModule configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: 'auth_jwt_secret',
+      signOptions: { expiresIn: '1h'}
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
