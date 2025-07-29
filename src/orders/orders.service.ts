69b7bf48b19e7bfa68b4b865fc45be248146e303
Nelya <nelya0816@gmail.com>
2025-07-30 01:32:54 +0300

    Add quantity limit validation for order items

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Orders } from 'src/schemas/orders';
@@ -8,6 +8,11 @@ export class OrdersService {
   constructor(@InjectModel(Orders.name) private orderModel: Model<Orders>) {}
 
   async create(orderData: any): Promise<Orders> {
+    for (const item of orderData.cart) {
+      if (item.quantity > 10) {
+        throw new BadRequestException('Максимум 10 пицц одного вида в заказе');
+      }
+    }
     const createdOrder = new this.orderModel(orderData);
     return createdOrder.save();
   }
